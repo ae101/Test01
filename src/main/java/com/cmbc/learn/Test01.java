13e6df60759ecadd37d0d7464797a17e235b746d
ae <1141239384@qq.com>
2019-09-19 11:41:35 +0800

    Initial Commit

---
+++ src/main/java/com/cmbc/learn/Test01.java
@@ -0,0 +1,10 @@
+package com.cmbc.learn;
+
+/**
+ * Created by ae on 2019/9/19.
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
