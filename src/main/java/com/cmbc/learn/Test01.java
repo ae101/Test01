95d89799da5e6aae30606de50a1fc1b2555590dd
ae <1141239384@qq.com>
2019-09-19 16:23:53 +0800

    from workspace1

--- src/main/java/com/cmbc/learn/Test01.java
+++ src/main/java/com/cmbc/learn/Test01.java
@@ -7,5 +7,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Here we are");
+        System.out.println("hello from workspace1");
     }
 }
